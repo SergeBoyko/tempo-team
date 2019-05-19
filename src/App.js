241119237ea9e16983ce23436a95f12d9f465a25
Serge Boyko <you@example.com>
2019-05-19 17:18:05 -0400

    clear coding

--- src/App.js
+++ src/App.js
@@ -2,13 +2,10 @@ import React from 'react';
 import axios from 'axios'
 import { Route, Redirect, Switch } from "react-router-dom";
 
-
 import TeamGroup from './containers/Team/TeamGroup/TeamGroup';
 import './App.css';
 import SeachBox from './components/SearchBox';
 import NotFound from './components/NotFound/NotFound';
-import TeamList from './containers/Team/TeamList/TeamList';
-import TeamGroupDetails from './containers/Team/TeameGroupDetails/TeamGroupDetails';
 
 axios.defaults.baseURL = "http://tempo-test.herokuapp.com/7d1d085e-dbee-4483-aa29-ca033ccae1e4/1"
 
@@ -17,9 +14,6 @@ function App() {
     <div className="container">
       <SeachBox />
       <Switch>
-        {/* <Route path="/team/0" render={({ match }) => (
-          <Redirect to={`/team`} />
-        )} /> */}
         <Route path="/team/:id" component={TeamGroup} />
         <Route path="/team" component={TeamGroup} />
         <Redirect from="/" exact to="/team" />
